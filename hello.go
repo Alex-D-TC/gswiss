2668e1c12b166a4c5008b682f95457b5b0c8d179
Alexandru Duma <alexandru.duma@spyhce.com>
2018-08-03 10:49:24 +0300

    Implemented K-Tree without recursive branching

--- hello.go
+++ hello.go
@@ -1,47 +1,131 @@
 package main
 
 import (
-	"bufio"
-	"crypto/sha256"
-	"fmt"
 	"net"
 )
 
 func main() {
 }
 
-func startServer() {
+type KTree struct {
+	left  *innerKTree
+	right *innerKTree
+	k     uint
+}
 
-	sv, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Started server")
-		for {
-			conn, err := sv.Accept()
-			if err != nil {
-				fmt.Println(err)
-			}
-			go handleConn(conn)
-		}
+type innerKTree struct {
+	left  *innerKTree
+	right *innerKTree
+	data  []NodeData
+}
+
+type NodeData struct {
+	dhtAddress []byte
+	netAddress net.Addr
+}
+
+func Make(k uint) KTree {
+	return KTree{
+		left:  makeInnerKTree(),
+		right: makeInnerKTree(),
+		k:     k,
 	}
 }
 
-func handleConn(conn net.Conn) {
-	defer conn.Close()
+func makeInnerKTree() *innerKTree {
+	return &innerKTree{
+		left:  nil,
+		right: nil,
+		data:  nil,
+	}
+}
 
-	reader := bufio.NewReader(conn)
+func (tree *KTree) Insert(data NodeData) {
+	var runningTree *innerKTree
+	var runningIdx uint = 1
+	dhtAddr := data.dhtAddress
+
+	if !getBit(dhtAddr, 0) {
+		runningTree = tree.left
+	} else {
+		runningTree = tree.right
+	}
+
 	for {
-		result, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
+		runningBit := getBit(dhtAddr, runningIdx)
+
+		if !runningBit && runningTree.left == nil ||
+			runningBit && runningTree.right == nil {
+			runningTree.data = append(runningTree.data, data)
+
+			// TODO: Add recursive branching if the K-Bucket is full
+			break
+		} else {
+
+			if !runningBit {
+				runningTree = runningTree.left
+			} else {
+				runningTree = runningTree.right
+			}
+		}
+
+		runningIdx++
+	}
+}
+
+func (tree *KTree) LocateClosest(data []byte) []NodeData {
+	var runningTree *innerKTree
+	var runningIdx uint = 1
+
+	if !getBit(data, 0) {
+		runningTree = tree.left
+	} else {
+		runningTree = tree.right
+	}
+
+	for {
+		runningBit := getBit(data, runningIdx)
+
+		if !runningBit {
+
+			if runningTree.left == nil {
+				return runningTree.data
+			}
+
+			runningTree = runningTree.left
+
+		} else {
+
+			if runningTree.right == nil {
+				return runningTree.data
+			}
+
+			runningTree = runningTree.right
+		}
+
+		runningIdx++
+	}
+
+}
+
+func getBit(data []byte, index uint) bool {
+	byteIdx := index / 8
+	return getFromByte(data[byteIdx], uint8(index%8))
+}
+
+func getFromByte(data byte, index uint8) bool {
+	var startMask byte = 1
+	var idx uint8 = 7
+
+	for {
+		if idx == index {
 			break
 		}
-		fmt.Println(result)
+
+		idx--
+		startMask = startMask << 1
 	}
 
-}
-
-func computeID(data []byte) [sha256.Size]byte {
-	return sha256.Sum256(data)
+	bit := data & startMask
+	return !(bit == 0)
 }
